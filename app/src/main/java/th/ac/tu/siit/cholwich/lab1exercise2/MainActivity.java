e0757e0f5a135f1db2dc80e4a9d9f7acb4724cab
herinkim <cookie0303@hotmail.com>
2015-01-19 14:44:00 +0700

    Successfully convert temperature from 3 type

--- app/src/main/java/th/ac/tu/siit/cholwich/lab1exercise2/MainActivity.java
+++ app/src/main/java/th/ac/tu/siit/cholwich/lab1exercise2/MainActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -35,5 +39,73 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void convertTemperature(View v) {
+
+        // Get the user input to convert the temperature
+        EditText etInput = (EditText) findViewById(R.id.etInput);
+        String s = etInput.getText().toString();
+        double inputVal = Double.parseDouble(s);
+
+
+        // Get the input temperature type
+        RadioGroup rgFrom = (RadioGroup) findViewById(R.id.rgFrom);
+        int convertFrom = rgFrom.getCheckedRadioButtonId();
+
+        // Get the output temperature type
+        RadioGroup rgTo = (RadioGroup) findViewById(R.id.rgTo);
+        int convertTo = rgTo.getCheckedRadioButtonId();
+
+        double result = inputVal;
+
+        // Check what the input temperature type is
+        if (convertFrom == R.id.rbFrmC)
+        {
+            // Convert from Celsius
+            if(convertTo == R.id.rbToF)
+            {
+                // Convert to Fahrenheit
+                result = inputVal * (9.0/5.0) + 32;
+            }
+            else if(convertTo == R.id.rbToK)
+            {
+                // Convert to Kelvin
+                result = inputVal + 273.15;
+            }
+        }
+        else if(convertFrom == R.id.rbFrmF)
+        {
+            // Convert from Fahrenheit
+            if(convertTo == R.id.rbToC)
+            {
+                // Convert to Celsius
+                result = (inputVal-32)*(5.0/9.0);
+            }
+            else if(convertTo == R.id.rbToK)
+            {
+                // Convert to Kelvin
+                result = (inputVal + 459.67)*(5.0/9.0);
+            }
+
+        }
+        else if (convertFrom == R.id.rbFrmK)
+        {
+            // Convert from Kelvin
+            if(convertTo == R.id.rbToC)
+            {
+                // Convert to Celsius
+                result = inputVal - 273.15;
+
+            }
+            else if(convertTo == R.id.rbToF)
+            {
+                // Convert to Fahrenheit
+                result = inputVal*(5.0/9.0) -459.67;
+            }
+        }
+
+        TextView t = (TextView) findViewById(R.id.tvOutput);
+        t.setText(Double.toString(result));
     }
 }
